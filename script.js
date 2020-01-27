3adcbb6e49e9bf5a5fbc988ebe218cee46e790ef
Janile Dantas <janille_dantas@hotmail.com>
2020-01-27 19:54:30 -0300

    An Introduction - JS Patterns - Parte I

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Function as abstraction */
+/* 
+    Creo una variable que apunta a una función.
+    El objetivo de esta función en abstraer cualquier tipo de operación que deba realizarse 
+*/
+var work = function() {
+    console.log("Working hard!");
+}
+
+work();
