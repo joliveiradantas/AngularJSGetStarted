088a3a08f228daacbdee56112e4ac1febf5a9b9e
Janile Dantas <janille_dantas@hotmail.com>
2020-01-27 20:38:47 -0300

    An Introduction - Modules - Parte I

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-/* Function as abstraction */
+/* Function to build modules */
 /* 
-    Elevando el nivel de abstracción, porque cuando se programa con Angular vamos a necesitar
-    definir una función que ejecute algo de trabajo o un algoritmo o que cree alguna estructura
-    de datos, y necesitará tomar esa función y pasar a Angular porque necesitará de esta función
-    y su abstracción para ejecutar algo en su nombre.  
-    Que hay de utilidad? Porque no invocar directamente work()?
-    La idea es proporcionar abstracción.
-    Probablemente doWork() represente alguna rutina, con lógica, que indica cuando el trabajo 
-    va a empezar y cuando va a terminar, por ejemplo. O incluso hay que realizar alguna operación peligrosa
-    y podemos incluir un try y catch. Por ende, doWork se convierte en una función genérica.
+    Utilizar un objeto que expone métodos y datos, por ejemplo necesitamos a 
+    worker object y puedo obtener ese objeto invocando a una función createWorker().
+    Una vez que tenga al worker puedo hacer cosas como: worker.job1(), worker.job2()
+    No nos interesa qué son esos trabajos. job1() podría llamar a un servidor.
+    Hay varias maneras de crear ese objeto que tiene 2 métodos, con JS. Pero vamos a ver
+    el enfoque que se utiliza en Angular
 */
-var work = function() {
-    console.log("Working hard!");
-}
 
-//El parámetro "f" es otra función
-var doWork = function(f) {
-    
-    console.log("starting!");
+var createWorker = function() {
 
-    try {
-        f();    
-    } 
-    catch (error) {   
-        console.log(error);     
-    }    
+    //sintaxis literal de JS para devolver un objeto con algún método asociado
+    return {
+        job1: function(){
 
-    console.log("end");
-}
+        }
+    };
+};
 
-//No estamos invocando "work"
-doWork(work);
+var worker = createWorker();
+worker.job1();
+worker.job2();
