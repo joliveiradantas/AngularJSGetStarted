39d70b9c70263ddcd340c0a976b4a47400b90e91
Janile Dantas <janille_dantas@hotmail.com>
2020-01-28 00:46:54 -0300

    Controllers - Controller Basics

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
-/* IIFE */
-/*
-    La función createWorker crea en JS lo que se conoce como escopo. Las variables que hemos 
-    creado adentro de esa función sólo son visible adentro de ellas.
-    Pero createWorker y worker son variables globales porque están definidas afuera de cualquier
-    otro código, afuera de cualquier otra función.
-    En JS las variables globales son muy malas, porque JS es un lenguaje dinámico y es muy fácil 
-    anular una variables global.
-    Como evitar las variables globales?
-*/
+//"MainCtrl" es una variable global (la estoy definiendo afuera de cualquier otro código)
+var MainCtrl = function($scope) {
 
-//Immediately invoked function expression
-(function(){
-    var createWorker = function() {
-
-        var workCount = 0;
-        
-        var task1 = function(){
-            workCount += 1;
-            console.log("task 1 " + workCount);
-        };
-        
-        var task2 = function(){
-            workCount += 1;
-            console.log("task 2 " + workCount);
-        };
-        
-        return {
-            job1: task1,
-            job2: task2
-        }
-    };
-
-    var worker = createWorker();
-    worker.job1();
-    worker.job2();
-    worker.job2();
-    worker.job2();
-}());
-
-/* 
-    Todo el código está dentro de una función y no estamos creando ninguna variable
-    global. Se utiliza IIFE para controlar el alcance de las variables, para construir
-    módulos para propoercionar encapsulamiento.
-*/
+    //Tenemos al modelo adjunto al $scope y se mostrará en la vista
+    //gracias a las binding expression
+    /* La palabra $scope tiene un sentido porque realmente determina el escopo
+        de donde las data binding expression se evalúan. Si Angular encuentra {{message}} en el HTML
+        va a ir a buscar en el objeto $scope del controller asociado a esa determinada area del HTML
+    */
+    $scope.message = "Hello, Angular!";
+};
