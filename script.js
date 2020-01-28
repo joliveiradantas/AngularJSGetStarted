0e9be412120b2b34c8bf9e61c9c6377f1376b43b
Janile Dantas <janille_dantas@hotmail.com>
2020-01-27 21:31:22 -0300

    An Introduction - IIFE - Parte I

--- script.js
+++ script.js
@@ -1,52 +1,40 @@
-/* Function to build modules */
-/* 
-    Utilizar un objeto que expone métodos y datos, por ejemplo necesitamos a 
-    worker object y puedo obtener ese objeto invocando a una función createWorker().
-    Una vez que tenga al worker puedo hacer cosas como: worker.job1(), worker.job2()
-    No nos interesa qué son esos trabajos. job1() podría llamar a un servidor.
-    Hay varias maneras de crear ese objeto que tiene 2 métodos, con JS. Pero vamos a ver
-    el enfoque que se utiliza en Angular
+/* IIFE */
+/*
+    La función createWorker crea en JS lo que se conoce como escopo. Las variables que hemos 
+    creado adentro de esa función sólo son visible adentro de ellas.
+    Pero createWorker y worker son variables globales porque están definidas afuera de cualquier
+    otro código, afuera de cualquier otra función.
+    En JS las variables globales son muy malas, porque JS es un lenguaje dinámico y es muy fácil 
+    anular una variables global.
+    Como evitar las variables globales?
 */
+var program  = function(){
+    var createWorker = function() {
 
-/* Revealing Module Pattern
-    Es módulo porque encapsulamos algún código adentro de una función.
-    Módulo en el desarrollo de software es una colección de componentes y características
-    que puedes juntar para realizar un trabajo útil.
-    Estamos creando un módulo en JS definiendo una función, dentro de esa función construimos algunas
-    cosas que podemos usar y exponer al mundo exterior e incluso tener algunos detalles privados
-    de implementación.    
-*/
+        var workCount = 0;
+        
+        var task1 = function(){
+            workCount += 1;
+            console.log("task 1 " + workCount);
+        };
+        
+        var task2 = function(){
+            workCount += 1;
+            console.log("task 2 " + workCount);
+        };
 
-var workCount = 0;
-
-var createWorker = function() {
-
-    //Cuando me pongo a escribir un objeto que va a exponer a algun tipo de API hay que
-    //centrarse en los detalles de la implementación
-
-    //Detalles privados de la implementación
-    var task1 = function(){
-        workCount += 1;
-        console.log("task 1 " + workCount);
-    };
-    
-    var task2 = function(){
-        workCount += 1;
-        console.log("task 2 " + workCount);
+        
+        return {
+            job1: task1,
+            job2: task2
+        }
     };
 
-    //Como devuelvo un objeto que expone task1 y task2?
-    return {
-        job1: task1,
-        job2: task2
-    }
-};
+    var worker = createWorker();
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job2();
+}
 
-var worker = createWorker();
-worker.job1();
-worker.job2();
-worker.job2();
-Worker.job2();
-
-/* Esto es lo que haremos con Angular, definir una función que devuelve un objeto
-que proporciona una API u otras piezas de código en otra parte de la app para usar */
+program();
