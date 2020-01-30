eaaf639675788876870489b704f8f4cc5f23fb6e
JOLIVEIR <janile.dantas@sbi-technology.com>
2020-01-30 15:02:13 -0300

    Controllers - Creating  a Module - Part I

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-//Pedimos el servicio $http a Angular para llamar a la API de GitHub
-var MainCtrl = function($scope, $http) {
 
-    var onUserComplete = function(response){
-        $scope.user = response.data;
+(function() {
+
+    //forma de definir un módulo pasando una matriz vacia para decir que no tiene
+    //dependencias con otros módulos
+    var app = angular.module("gitHubViewer", [])
+
+    var MainCtrl = function($scope, $http) {
+
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+        };
+
+        var onError = function(reason){
+            $scope.error = "Could not fetch the user";
+        };    
+        
+        $http.get("https://api.github.com/users/robconery")
+                .then(onUserComplete, onError);
+    
+        $scope.message = "Hello, Angular!";
+        
     };
 
-    var onError = function(reason){
-        $scope.error = "Could not fetch the user";
-    };
-    
-    /* Si la URL no es correcta no aparecerá los datos en la pantalla
-       porque no se adjuntaron los datos en el objeto $scope, porque el
-       método then sólo si la llamada get es exitosa. Si hay un error puedo
-       llamar a una segunda funcion */
-    $http.get("https://ap.github.com/users/robconery")
-            .then(onUserComplete, onError);
-   
-    $scope.message = "Hello, Angular!";
-    
-};
+    app.controller("MainCtrl", MainCtrl)
+
+}())    
