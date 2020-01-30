a5f5125971d0fc4652e5b0dcc15bdb2df4f1be25
JOLIVEIR <janile.dantas@sbi-technology.com>
2020-01-30 14:28:57 -0300

    Controllers - Using $http

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var MainCtrl = function($scope) {
+//Pedimos el servicio $http a Angular para llamar a la API de GitHub
+var MainCtrl = function($scope, $http) {
 
-    var person = {
-        firstName: "Scott",
-        lastName: "Allen",
-        imageSrc: "http://odetocode.com/Images/scott_allen_2.jpg"
+    var onUserComplete = function(response){
+        $scope.user = response.data;
     };
 
+    var onError = function(reason){
+        $scope.error = "Could not fetch the user";
+    };
+    
+    /* Si la URL no es correcta no aparecerá los datos en la pantalla
+       porque no se adjuntaron los datos en el objeto $scope, porque el
+       método then sólo si la llamada get es exitosa. Si hay un error puedo
+       llamar a una segunda funcion */
+    $http.get("https://ap.github.com/users/robconery")
+            .then(onUserComplete, onError);
+   
     $scope.message = "Hello, Angular!";
-    $scope.person = person; 
+    
 };
