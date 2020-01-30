7d45b80eb637c43ba470544da02fba6688ed11ab
Janile Dantas <janille_dantas@hotmail.com>
2020-01-30 01:04:40 -0300

    Controllers - The capabilities

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//"MainCtrl" es una variable global (la estoy definiendo afuera de cualquier otro código)
 var MainCtrl = function($scope) {
 
-    //Tenemos al modelo adjunto al $scope y se mostrará en la vista
-    //gracias a las binding expression
-    /* La palabra $scope tiene un sentido porque realmente determina el escopo
-        de donde las data binding expression se evalúan. Si Angular encuentra {{message}} en el HTML
-        va a ir a buscar en el objeto $scope del controller asociado a esa determinada area del HTML
-    */
+    var person = {
+        firstName: "Scott",
+        lastName: "Allen",
+        imageSrc: "http://odetocode.com/Images/scott_allen_2.jpg"
+    };
+
     $scope.message = "Hello, Angular!";
+    $scope.person = person; 
 };
