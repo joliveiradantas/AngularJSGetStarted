9416580e4f388c25753d3ffb6c5808cf231c3e18
Janile Dantas <janille_dantas@hotmail.com>
2020-01-30 19:45:59 -0300

    Directives and Views - ng-repeat

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 
 (function() {
-
-    //La forma de definir un módulo pasando una matriz vacia para decir que no tiene
-    //dependencias con otros módulos
+    
     var app = angular.module("gitHubViewer", [])
 
     var MainCtrl = function($scope, $http) {
 
         var onUserComplete = function(response){
             $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                    .then(onRepos, onError);
+        };
+
+        var onRepos = function(response){
+            //Podría también utilizar una variable como $scope.user.repos
+            $scope.repos = response.data;
         };
 
         var onError = function(reason){
-            $scope.error = "Could not fetch the user";
+            $scope.error = "Could not fetch the data";
         };  
         
         $scope.search = function(username){
